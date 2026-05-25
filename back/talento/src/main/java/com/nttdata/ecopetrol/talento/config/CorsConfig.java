d638379a43cd96119e0622452e7b8737f97d1cd4
Ivan de la rosa <idelarop@emeal.nttdata.com>
2026-05-25 09:08:53 -0500

    exponer headers X-Session-ID

--- back/talento/src/main/java/com/nttdata/ecopetrol/talento/config/CorsConfig.java
+++ back/talento/src/main/java/com/nttdata/ecopetrol/talento/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
+                .exposedHeaders("X-Session-ID")
                 .allowCredentials(false);
     }
 }
